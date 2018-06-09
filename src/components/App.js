14e5ab03eb38299b5d99868ba5480db9c88642b6
Mark <mtwmark2010@hotmail.co.uk>
2018-06-09 10:44:00 +0100

    got app working - ready for redux intro

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,19 @@
-import React, {PropTypes} from 'react'
+import React, {PropTypes} from 'react';
+import Header from "./common/Header";
 
 class App extends React.Component{
     render() {
         return (
             <div className="container-fluid">
-                {this.props.children}
+              <Header />
+              {this.props.children}
             </div>
         );
     }
 }
 
-App.prototype = {
+App.propTypes = {
     children: PropTypes.object.isRequired
-}
+};
 
 export default App;
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import {Route, IndexRoute} from 'react-router'
+import React from 'react';
+import {Route, IndexRoute} from 'react-router';
 import App from './components/App';
 import HomePage from "./components/home/HomePage";
 import AboutPage from "./components/about/AboutPage";
+import CoursesPage from "./components/course/CoursesPage";
+import AdminPage from "./components/admin/AdminPage";
 
 export default (
     <Route path="/" component={App}>
-        <IndexRoute component={HomePage} />
-        <Route path="about" component={AboutPage} />>
+      <IndexRoute component={HomePage} />
+      <Route path="about" component={AboutPage} />
+      <Route path="courses" component={CoursesPage} />
+      <Route path="admin" component={AdminPage} />
     </Route>
 );
