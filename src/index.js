39b496d27033026d07fb62f6c95f0920ddd5cdc8
Mark <mtwmark2010@hotmail.co.uk>
2018-06-12 21:08:59 +0100

    finished off thunk

--- src/index.js
+++ src/index.js
@@ -7,13 +7,15 @@ import  './styles/styles.css';
 import  '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import configureStore from './store/configureStore';
 import {Provider}  from 'react-redux';
+import {loadCourses} from "./actions/courseAction";
 
 const store = configureStore();
+store.dispatch(loadCourses());
 
 render(
   <Provider store={store}>
     <Router history={browserHistory} routes={routes}/>
-  </Provider>,  
+  </Provider>,
   document.getElementById('app')
 );
 
