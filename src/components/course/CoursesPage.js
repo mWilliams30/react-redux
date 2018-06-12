ebfa3dc76039818f47be3652813f9894c165e673
mark.williams <mark.williams@TCN124R-PC>
2018-06-12 09:01:07 +0100

    connected course page with redux

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,4 +1,6 @@
-import React, {PropTypes} from "react";
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import * as courseActions from '../../actions/courseAction';
 
 class CoursesPage extends React.Component{
   constructor(props, context){
@@ -19,7 +21,7 @@ class CoursesPage extends React.Component{
   }
 
   onClickSave() {
-    alert(`Saving ${this.state.course.title}`);
+    this.props.dispatch(courseActions.createCourse(this.state.course));
   }
 
   render(){
@@ -34,4 +36,10 @@ class CoursesPage extends React.Component{
   }
 }
 
-export default CoursesPage;
+mapStateToProps = () => {
+  return{
+    courses: state.courses
+  };
+}
+
+export default connect(mapStateToProps)(CoursesPage);
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,10 +2,10 @@ import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutableStateInvariantMiddleware from 'redux-immutable-state-invariant';
 
-export default  (initialState) => {
+export default (initialState) => {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(reduxImmutableStateInvariantMiddleware)
+    applyMiddleware(reduxImmutableStateInvariantMiddleware())
   );
 };
